7e6fa4577a05e9dcf12018d254ed1ccf4e2cb8b7
Nitczi <51859779+Nitczi@users.noreply.github.com>
2023-11-17 21:23:04 -0300

    Create hello_friend.asm

---
+++ hello_world/hello_friend.asm
@@ -0,0 +1,16 @@
+section .data
+  message: db "Hello friend!", 0xa
+
+section .text
+global _start
+
+_start:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, message
+  mov rdx, 14
+  syscall
+
+  mov rax, 0x3c
+  xor rdi, rdi
+  syscall
